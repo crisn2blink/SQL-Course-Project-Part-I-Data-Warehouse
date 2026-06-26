09cdb84a961052c45b14fb6ce8c772b82d9773c2
Cristian Cervantes <crisn2blink@gmail.com>
2026-06-26 13:54:32 -0500

    Update 09_performance_analysis.sql

--- 02-eda/queries/09_performance_analysis.sql
+++ 02-eda/queries/09_performance_analysis.sql
@@ -14,12 +14,12 @@ SQL Functions Used:
 ===============================================================================
 */
 
-/* Analyze the yearly performance of products by comparing their sales 
-to both the average sales performance of the product and the previous year's sales */
-WITH yearly_product_sales AS (
+/* Analyze the yearly performance of categories by comparing their sales 
+to both the average sales performance of the category and the previous year's sales */
+WITH yearly_category_sales AS (
     SELECT
         YEAR(f.order_date) AS order_year,
-        p.product_name,
+        p.category,
         SUM(f.sales_amount) AS current_sales
     FROM gold.fact_sales f
     LEFT JOIN gold.dim_products p
@@ -27,26 +27,26 @@ WITH yearly_product_sales AS (
     WHERE f.order_date IS NOT NULL
     GROUP BY 
         YEAR(f.order_date),
-        p.product_name
+        p.category
 )
 SELECT
     order_year,
-    product_name,
+    category,
     current_sales,
-    AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales,
-    current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
+    AVG(current_sales) OVER (PARTITION BY category) AS avg_sales,
+    current_sales - AVG(current_sales) OVER (PARTITION BY category) AS diff_avg,
     CASE 
-        WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
-        WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
+        WHEN current_sales - AVG(current_sales) OVER (PARTITION BY category) > 0 THEN 'Above Avg'
+        WHEN current_sales - AVG(current_sales) OVER (PARTITION BY category) < 0 THEN 'Below Avg'
         ELSE 'Avg'
     END AS avg_change,
     -- Year-over-Year Analysis
-    LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS py_sales,
-    current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS diff_py,
+    LAG(current_sales) OVER (PARTITION BY category ORDER BY order_year) AS py_sales,
+    current_sales - LAG(current_sales) OVER (PARTITION BY category ORDER BY order_year) AS diff_py,
     CASE 
-        WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
-        WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+        WHEN current_sales - LAG(current_sales) OVER (PARTITION BY category ORDER BY order_year) > 0 THEN 'Increase'
+        WHEN current_sales - LAG(current_sales) OVER (PARTITION BY category ORDER BY order_year) < 0 THEN 'Decrease'
         ELSE 'No Change'
     END AS py_change
-FROM yearly_product_sales
-ORDER BY product_name, order_year;
+FROM yearly_category_sales
+ORDER BY category, order_year;
